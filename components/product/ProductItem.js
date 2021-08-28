3d5141cc089a0b1714c190675c6d2572c314f281
Ricky <phangr28@gmail.com>
2021-08-28 16:17:52 +0700

    product

---
+++ components/product/ProductItem.js
@@ -0,0 +1,63 @@
+import Link from "next/link";
+
+const ProductItem = ({ product }) => {
+	const userLink = () => {
+		return (
+			<>
+				<Link href={`product/${product._id}`}>
+					<a
+						className="btn btn-info"
+						style={{ marginRight: "5px", flex: 1 }}
+					>
+						View
+					</a>
+				</Link>
+				<button
+					className="btn btn-success"
+					style={{ marginLeft: "5px", flex: 1 }}
+				>
+					Buy
+				</button>
+			</>
+		);
+	};
+
+	return (
+		<div className="card" style={{ width: "18rem" }}>
+			<img
+				src={product.images[0].url}
+				className="card-img-top"
+				alt={product.images[0].url}
+			/>
+			<div className="card-body">
+				<h5
+					className="card-title text-capitalize"
+					title={product.title}
+				>
+					{product.title}
+				</h5>
+
+				<div className="d-flex justify-content-between mx-0">
+					<h6 className="text-danger">${product.price}</h6>
+					{product.inStock > 0 ? (
+						<h6 className="text-danger">
+							In Stock: {product.inStock}
+						</h6>
+					) : (
+						<h6 className="text-danger">Out Stock</h6>
+					)}
+				</div>
+
+				<p className="card-text" title={product.description}>
+					{product.description}
+				</p>
+
+				<div className="d-flex justify-content-between mx-0">
+					{userLink()}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProductItem;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
-const Home = () => {
-    return (
-        <div>
-            <p>Home</p>
-        </div>
-    );
+import Head from "next/head";
+import { useState } from "react";
+import { getData } from "../utils/fetchData";
+import ProductItem from "../components/product/ProductItem";
+
+const Home = (props) => {
+	const [products, setProducts] = useState(props.product);
+
+	console.log(products);
+
+	return (
+		<div className="products">
+			<Head>
+				<title>Home Page</title>
+			</Head>
+
+			{products.length === 0 ? (
+				<h2>No Products</h2>
+			) : (
+				products.map((product) => (
+					<ProductItem key={product._id} product={product} />
+				))
+			)}
+		</div>
+	);
 };
 
 export default Home;
+
+export async function getServerSideProps() {
+	const res = await getData("product");
+
+	return {
+		props: {
+			product: res.products,
+			result: res.result,
+		},
+	};
+}
